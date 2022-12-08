fce9654ba8ac3e89e3d543e47fe2f7ee3f1fd94f
michaelrmartin <iammichaelmartin@gmail.com>
2022-12-08 17:06:01 -0600

    Adds UserCreateView.vue and associated routes in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import UserCreateView from "../views/UserCreateView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/usercreate",
+    name: "usercreate",
+    component: UserCreateView,
   },
   {
     path: "/about",
