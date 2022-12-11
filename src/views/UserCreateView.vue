083bf6c1985ab1cfd2532fedf7848a9478970b9a
michaelrmartin <iammichaelmartin@gmail.com>
2022-12-11 16:31:49 -0600

    Updates styling

--- src/views/UserCreateView.vue
+++ src/views/UserCreateView.vue
@@ -9,7 +9,6 @@ export default {
       options: {},
       errors: [],
       successMessage: "",
-      errorMessage: "",
     };
   },
   created: function () {
@@ -21,13 +20,10 @@ export default {
         .get("/form")
         .then((response) => {
           this.options = response.data;
-          console.log(response);
-          console.log("Options:", response.data);
         })
         .catch((error) => {
           console.log(error);
           this.errors = error.response.data.errors;
-          this.errorMessage = error.response.statusText;
         });
     },
     createUser: function () {
@@ -36,7 +32,7 @@ export default {
         .then((response) => {
           console.log(response);
           console.log("Success:", response.data);
-          this.message = "User successfully created!";
+          this.successMessage = "User successfully created!";
           this.newUserParams = {};
         })
         .catch((error) => {
@@ -49,56 +45,58 @@ export default {
 </script>
 
 <template>
-  <div class="container" style="width: 60%">
-    <form v-on:submit.prevent="createUser()">
-      <h1>Create User</h1>
-      <div class="form-group mb-3">
-        <label for="signupNameInput" class="form-label">Name</label>
-        <input type="text" class="form-control" id="signupNameInput" v-model="newUserParams.name" required />
-      </div>
-      <div class="form-group mb-3">
-        <label for="signupEmailInput" class="form-label">Email</label>
-        <input type="email" class="form-control" id="signupEmailInput" v-model="newUserParams.email" required />
-      </div>
-      <div class="form-group mb-3">
-        <label for="signupPasswordInput" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="signupPasswordInput"
-          v-model="newUserParams.password"
-          required
-        />
-      </div>
-      <div class="form-group mb-3">
-        <label for="signupOccupationInput" class="form-label">Occupation</label>
-        <select class="form-control" id="signupOccupationInput" v-model="newUserParams.occupation" required>
-          <option v-for="occupation in options.occupations" v-bind:key="occupation.id">{{ occupation }}</option>
-        </select>
-      </div>
-      <div class="form-group mb-3">
-        <label for="signupStateInput" class="form-label">State</label>
-        <select class="form-control" id="signupStateInput" v-model="newUserParams.state" required>
-          <option v-for="state in options.states" v-bind:key="state.id">{{ state.name }}</option>
-        </select>
-      </div>
-      <ul v-if="errorMessage">
-        <li>{{ errorMessage }}</li>
-      </ul>
-      <br />
-      <button type="submit" class="btn btn-secondary">Submit</button>
-      <br />
-      <br />
-      <h2 v-if="createUser && successMessage">{{ successMessage }}</h2>
-    </form>
-  </div>
+  <h1>Create User</h1>
+  <br />
+  <form class="row g-3" v-on:submit.prevent="createUser()">
+    <div class="mb-3 col-md-12">
+      <label for="signupNameInput" class="form-label">Name</label>
+      <input type="text" class="form-control" id="signupNameInput" v-model="newUserParams.name" required />
+    </div>
+    <div class="mb-3 col-6">
+      <label for="signupEmailInput" class="form-label">Email</label>
+      <input type="email" class="form-control" id="signupEmailInput" v-model="newUserParams.email" required />
+    </div>
+    <div class="mb-3 col-6">
+      <label for="signupPasswordInput" class="form-label">Password</label>
+      <input type="password" class="form-control" id="signupPasswordInput" v-model="newUserParams.password" required />
+    </div>
+    <div class="mb-3 col-8">
+      <label for="signupOccupationInput" class="form-label">Occupation</label>
+      <select class="form-control" id="signupOccupationInput" v-model="newUserParams.occupation" required>
+        <option v-for="occupation in options.occupations" v-bind:key="occupation.id">{{ occupation }}</option>
+      </select>
+    </div>
+    <div class="mb-3 col-4">
+      <label for="signupStateInput" class="form-label">State</label>
+      <select class="form-control" id="signupStateInput" v-model="newUserParams.state" required>
+        <option v-for="state in options.states" v-bind:key="state.id">{{ state.name }}</option>
+      </select>
+    </div>
+    <ul>
+      <li v-for="error in errors" v-bind:key="error">Error: {{ error }}</li>
+    </ul>
+    <br />
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary col-4">Create</button>
+    </div>
+  </form>
+  <br />
+  <h2 v-if="createUser && successMessage">{{ successMessage }}</h2>
 </template>
 
 <style>
+form {
+  padding-left: 20%;
+  padding-right: 20%;
+}
+
 li {
   list-style-type: none;
   color: red;
   font-size: large;
   font-weight: bold;
+  border: red;
+  border-style: double;
+  text-justify: auto;
 }
 </style>
