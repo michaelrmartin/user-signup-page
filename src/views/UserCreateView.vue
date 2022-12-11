33c7837f9b87fdfd302d859764898eaffaa9deee
michaelrmartin <iammichaelmartin@gmail.com>
2022-12-11 17:36:26 -0600

    Update readme.md

--- src/views/UserCreateView.vue
+++ src/views/UserCreateView.vue
@@ -47,41 +47,54 @@ export default {
 <template>
   <h1>Create User</h1>
   <br />
-  <form class="row g-3" v-on:submit.prevent="createUser()">
-    <div class="mb-3 col-md-12">
-      <label for="signupNameInput" class="form-label">Name</label>
-      <input type="text" class="form-control" id="signupNameInput" v-model="newUserParams.name" required />
-    </div>
-    <div class="mb-3 col-6">
-      <label for="signupEmailInput" class="form-label">Email</label>
-      <input type="email" class="form-control" id="signupEmailInput" v-model="newUserParams.email" required />
-    </div>
-    <div class="mb-3 col-6">
-      <label for="signupPasswordInput" class="form-label">Password</label>
-      <input type="password" class="form-control" id="signupPasswordInput" v-model="newUserParams.password" required />
-    </div>
-    <div class="mb-3 col-8">
-      <label for="signupOccupationInput" class="form-label">Occupation</label>
-      <select class="form-control" id="signupOccupationInput" v-model="newUserParams.occupation" required>
-        <option v-for="occupation in options.occupations" v-bind:key="occupation.id">{{ occupation }}</option>
-      </select>
-    </div>
-    <div class="mb-3 col-4">
-      <label for="signupStateInput" class="form-label">State</label>
-      <select class="form-control" id="signupStateInput" v-model="newUserParams.state" required>
-        <option v-for="state in options.states" v-bind:key="state.id">{{ state.name }}</option>
-      </select>
-    </div>
+  <div class="container">
+    <form class="row g-3" v-on:submit.prevent="createUser()">
+      <div class="mb-3 col-md-12">
+        <label for="signupNameInput" class="form-label">Name</label>
+        <input type="text" class="form-control" id="signupNameInput" v-model="newUserParams.name" required />
+      </div>
+      <div class="mb-3 col-6">
+        <label for="signupEmailInput" class="form-label">Email</label>
+        <input type="email" class="form-control" id="signupEmailInput" v-model="newUserParams.email" required />
+      </div>
+      <div class="mb-3 col-6">
+        <label for="signupPasswordInput" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="signupPasswordInput"
+          v-model="newUserParams.password"
+          required
+        />
+        <small class="text-danger" v-if="newUserParams.password && newUserParams.password.length < 6">
+          Must be at least 6 characters
+        </small>
+        <small class="text-danger" v-if="newUserParams.password && newUserParams.password.length > 20">
+          Must not be over 20 characters
+        </small>
+      </div>
+      <div class="mb-3 col-8">
+        <label for="signupOccupationInput" class="form-label">Occupation</label>
+        <select class="form-control" id="signupOccupationInput" v-model="newUserParams.occupation" required>
+          <option v-for="occupation in options.occupations" v-bind:key="occupation.id">{{ occupation }}</option>
+        </select>
+      </div>
+      <div class="mb-3 col-4">
+        <label for="signupStateInput" class="form-label">State</label>
+        <select class="form-control" id="signupStateInput" v-model="newUserParams.state" required>
+          <option v-for="state in options.states" v-bind:key="state.id">{{ state.name }}</option>
+        </select>
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary col-4">Create</button>
+      </div>
+    </form>
+    <br />
     <ul>
       <li v-for="error in errors" v-bind:key="error">Error: {{ error }}</li>
     </ul>
-    <br />
-    <div class="col-12">
-      <button type="submit" class="btn btn-primary col-4">Create</button>
-    </div>
-  </form>
-  <br />
-  <h2 v-if="createUser && successMessage">{{ successMessage }}</h2>
+    <h2 v-if="createUser && successMessage">{{ successMessage }}</h2>
+  </div>
 </template>
 
 <style>
